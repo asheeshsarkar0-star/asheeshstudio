7735b02755eae89b72e9686a08e89bdbe88cef04
Asheesh Sarkar <asheeshsarkar0@gmail.com>
2026-01-14 19:02:44 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,38 @@
-const CACHE = "studio-v1";
+const CACHE = "studio-v2";
 
 const ASSETS = [
- "/",
- "/index.html",
- "/offline.html",
- "/manifest.json",
- "/icon-192.png",
- "/icon-512.png",
- "/banner.png",
- "/g1.png",
- "/g2.png"
+  "/",
+  "/index.html",
+  "/offline.html",
+  "/manifest.json",
+
+  "/file_0000000027e471faa86a9842ff80a567.png",   // NEW APP ICON
+
+  "/banner.png",
+  "/g1.png",
+  "/g2.png"
 ];
 
 self.addEventListener("install", e => {
- e.waitUntil(
-  caches.open(CACHE).then(cache => cache.addAll(ASSETS))
- );
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener("activate", e => {
- e.waitUntil(
-  caches.keys().then(keys =>
-   Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  )
- );
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    )
+  );
 });
 
 self.addEventListener("fetch", event => {
- event.respondWith(
-  caches.match(event.request).then(cached => {
-   return cached || fetch(event.request).catch(() => caches.match("/offline.html"));
-  })
- );
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).catch(() => caches.match("/offline.html"));
+    })
+  );
 });
